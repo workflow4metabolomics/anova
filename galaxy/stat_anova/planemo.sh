c351da4d7e642fa72331a279bafad6c18777ee7d
Gildas Le Corguillé <lecorguille@users.noreply.github.com>
2016-03-10 22:00:02 +0100

    svn2git

---
+++ galaxy/stat_anova/planemo.sh
@@ -0,0 +1,1 @@
+planemo shed_init -f --name=anova --owner=lecorguille --description="Anova 1 way\nFiltering on ajusted p.values" --homepage_url="http://workflow4metabolomics.org" --category="Statistics"
