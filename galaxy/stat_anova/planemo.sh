0921bc80e825d98964cf7a72c57facdb593ae355
Gildas Le Corguillé <lecorguille@users.noreply.github.com>
2016-03-13 15:25:35 +0100

    trigger travis

--- galaxy/stat_anova/planemo.sh
+++
@@ -1,1 +0,0 @@
-planemo shed_init -f --name=anova --owner=lecorguille --description="Anova 1 way\nFiltering on ajusted p.values" --homepage_url="http://workflow4metabolomics.org" --category="Statistics"
